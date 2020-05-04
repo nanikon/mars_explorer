caf25f5708d15be909ca758fc138d27d9b13c312
nanikon <milkchocolatewithdaisy@gmail.com>
2020-05-04 16:04:42 +0300

    second version (for task "Форма регистрации")

--- data/users.py
+++ data/users.py
@@ -1,6 +1,7 @@
 import datetime
 import sqlalchemy
 from .db_session import SqlAlchemyBase
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class User(SqlAlchemyBase):
@@ -17,3 +18,9 @@ class User(SqlAlchemyBase):
     email = sqlalchemy.Column(sqlalchemy.String, unique=True, nullable=True)
     hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     modified_date = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.datetime.now)
+
+    def set_password(self, password):
+        self.hashed_password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.hashed_password, password)
--- main.py
+++ main.py
@@ -4,8 +4,26 @@ from data import db_session
 from data.users import User
 from data.jobs import Jobs
 
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, IntegerField
+from wtforms.fields.html5 import EmailField
+from wtforms.validators import DataRequired
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+class RegisterForm(FlaskForm):
+    email = EmailField('Login / email', validators=[DataRequired()])
+    password = PasswordField('PasswordField', validators=[DataRequired()])
+    password_again = PasswordField('Repeat password', validators=[DataRequired()])
+    surname = StringField('Surname', validators=[DataRequired()])
+    name = StringField('Name', validators=[DataRequired()])
+    age = IntegerField('Age', validators=[DataRequired()])
+    position = StringField('Position', validators=[DataRequired()])
+    specially = StringField('Specially', validators=[DataRequired()])
+    address = StringField('Address', validators=[DataRequired()])
+    submit = SubmitField('Submit')
 
 
 @app.route('/')
@@ -13,6 +31,33 @@ def index():
     session = db_session.create_session()
     jobs = session.query(Jobs)
     return render_template('works.html', jobs=jobs)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    message = ''
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', form=form,
+                                   message="Пароли не совпадают")
+        session = db_session.create_session()
+        if session.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', form=form,
+                                   message="Такой пользователь уже есть")
+        user = User()
+        user.surname = form.surname.data
+        user.name = form.name.data
+        user.age = form.age.data
+        user.position = form.position.data
+        user.speciality = form.specially.data
+        user.address = form.address.data
+        user.email = form.email.data
+        user.set_password(form.password.data)
+        session.add(user)
+        session.commit()
+        message = 'Колонист ' + form.surname.data + ' ' + form.name.data + ' успешно занесен в нашу БД. Не желаете зарегистрировать ещё одного, пока мы готовим форму для авторизации?'
+    return render_template('register.html', form=form, message=message)
 
 
 if __name__ == '__main__':
